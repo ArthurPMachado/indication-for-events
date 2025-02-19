efbf9c3817d5b831fd6a1fd43544b95aae819ac4
Arthur Machado <machadoparthur1@gmail.com>
2025-02-19 20:38:53 -0300

    feat: fetch data for ranking

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -2,52 +2,37 @@ import Image from "next/image";
 import medalCooper from '../../../assets/medal-cooper.svg';
 import medalSilver from '../../../assets/medal-silver.svg';
 import medalGold from '../../../assets/medal-gold.svg';
+import { getRanking } from "@/http/api";
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking();
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking de indicações
       </h2>
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> |
-            Arthur Machado
-          </span>
+        {ranking.map((subscriber, index) => {
+          const rankingPosition = index + 1;
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <div key={subscriber.id} className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> | {' '}
+                {subscriber.name}
+              </span>
 
-          <Image src={medalGold} alt="" className="absolute top-0 right-8" />
-        </div>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {subscriber.score}
+              </span>
 
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> |
-            Arthur Machado
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image src={medalSilver} alt="" className="absolute top-0 right-8" />
-        </div>
-
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> |
-            Arthur Machado
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image src={medalCooper} alt="" className="absolute top-0 right-8" />
-        </div>
+              {rankingPosition === 1 && <Image src={medalGold} alt="" className="absolute top-0 right-8" />}
+              {rankingPosition === 2 && <Image src={medalSilver} alt="" className="absolute top-0 right-8" />}
+              {rankingPosition === 3 && <Image src={medalCooper} alt="" className="absolute top-0 right-8" />}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
